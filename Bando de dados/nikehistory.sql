1cec781aa0d9dc138aea4851a6bbbfee5c7af0e2
luangomesx <luan.araujo@sptech.school>
2023-11-27 23:57:55 -0300

    sql att

--- Bando de dados/nikehistory.sql
+++ Bando de dados/nikehistory.sql
@@ -21,9 +21,45 @@ constraint fkUs foreign key (fkUsuario)
     select * from usuario;
     
     select * from resultadoquiz;
-    
-select count(fkUsuario) from resultadoquiz where resultado > 20;
 
-    select count(resultado),
-    fkUsuario from resultadoquiz where resultado = 80 group by fkUsuario;
-    select count(resultado) from resultadoquiz where resultado = 80;
+-- qtd de usuarios acima da média
+    select count(fkUsuario) qtdUsuarios
+    from resultadoquiz where resultado > 60;
+
+-- qtd de usuarios na média
+    select count(fkUsuario) qtdUsuarios
+    from resultadoquiz where resultado = 60; 
+    
+    -- qtd de usuarios abaixo da média
+    select count(fkUsuario) qtdUsuarios
+    from resultadoquiz where resultado < 60; 
+    
+    -- usuarios com 20
+    select count(fkUsuario) qtdUsuarios from resultadoquiz
+    where resultado = 20;
+    
+    -- usuarios com 40
+    select count(fkusuario) qtdUsuarios
+    from resultadoquiz where resultado = 40;
+    
+    -- usuarios com 60
+    select count(fkusuario) qtdUsuarios
+    from resultadoquiz where resultado = 60;
+    
+    -- usuarios com 80
+    select count(fkusuario) qtdUsuarios
+    from resultadoquiz where resultado = 80;
+    
+    -- usuarios com 100
+    select count(fkusuario) qtdUsuarios
+    from resultadoquiz where resultado = 100;
+    
+    -- quantidade de usuarios da média pra baixo
+    select count(fkusuario) qtdUsuario,
+    resultado as resultado from resultadoquiz where resultado <= 60
+    group by resultado;
+    
+    -- quantidade de usuario da média pra cima
+    select count(fkusuario) qtdUsuario,
+    resultado as resultado from resultadoquiz where resultado >= 60
+    group by resultado; 
